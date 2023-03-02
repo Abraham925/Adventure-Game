bdb9c3e66b03604913bb95ab2ba324bc70fdf4cd
nathanmcfarland <nathan.mcfarland@my.wheaton.edu>
2023-03-02 13:21:05 -0600

    updated room and parser

--- game/Parser.java
+++ game/Parser.java
@@ -43,11 +43,12 @@ public class Parser {
         // The room that the user is in.
         Room room = game.getCurrentRoom();
 
-        System.out.println("You are in " + room.getDescription());
+        System.out.println(room.getDescription());
 
         System.out.print("Enter command--> ");
         String command = keyboard.nextLine().toLowerCase();  // user's command
         
+        //command list
         list[0] = "north";
         list[1] = "east";
         list[2] = "south";
@@ -55,61 +56,16 @@ public class Parser {
         list[4] = "up";
         list[5] = "down";
         
-        if (command.equals("north") || command.equals("south") 
-            || command.equals("west") || command.equals("east") 
-        	|| command.equals("up") || command.equals("down")) {
+        
         	if (room.getDir(command) == null) {
-                System.out.println("There is no room in that direction.");
-            }
-            if (command.equals("north")) {
-            	if(room.getDir("north") != null) {
-            		room = room.getDir("north");
-            	}
+                System.out.println("Nothing else exists that way.");
+            }else {
+            	room = room.getDir(command);
             	game.setCurrentRoom(room);
             }
-            else if (command.equals("south")) {
-            	if(room.getDir("south") != null) {
-            		room = room.getDir("south");
-            	}
-            	game.setCurrentRoom(room);
-            }
-            else if (command.equals("west")) {
-            	if(room.getDir("west") != null) {
-            		room = room.getDir("west");
-            	}
-            	game.setCurrentRoom(room);
-            }
-            else if (command.equals("east")) {
-            	if(room.getDir("east") != null) {
-            		room = room.getDir("east");
-            	}
-            	game.setCurrentRoom(room);
-            }
-            else if (command.equals("up")) { //added
-            	if(room.getDir("up") != null) {
-                	room = room.getDir("up");
-            		System.out.println("You went up the stairs");
-            	}
-            	game.setCurrentRoom(room);
-            }
-            else if (command.equals("down")) { //added
-            	if(room.getDir("down") != null) {
-                	room = room.getDir("down");
-            		System.out.println("You went down the stairs");
-            	}
-            	game.setCurrentRoom(room);
-            }
-            
-            
-            
-            
-        }else if(command.equals("help")) {
-        	help();
-        }
-        else
-            System.out.println("I do not know how to " + command + ".");
 
     }
+    
     
     public void help() {
     	for(int i = 0; i<list.length; i+=2) {
--- game/Room.java
+++ game/Room.java
@@ -16,15 +16,13 @@ import java.util.HashMap;
 public class Room {
 	
     /**
-     * Rooms adjacent to this one, to which there is a door.
-     */
-    //private Room north, south, east, west, above, below;
-
-    /**
      * A description of this room
      */
     private String description;
     
+    /**
+     * Holds the HashMap for a room, containing pairs of a direction and corresponding room
+     */
     private HashMap<String, Room> dir = new HashMap<String, Room>();
 
     /**
@@ -32,54 +30,28 @@ public class Room {
      * @param description A String describing this room to the user.
      */
     public Room(String description) { 
-    		this.description = description;
-    	}
-    
+    	this.description = description;
+    }
     
     /**
-     * Methods for added "doors"-- directional connections to other rooms.
+     * @param the inputed direction
+     * @return the room in that direction
      */
-    //public void setNorth(Room north) { this.north = north; }
-    //public void setSouth(Room south) { this.south = south; }
-    //public void setEast(Room east) { this.east = east; }
-    //public void setWest(Room west) { this.west = west; }
-    
     public Room getDir(String direction) {
 		return dir.get(direction);
 	}
 
-
+    /**
+     * @param the direction and the room in that direction
+     * @param a new room in that direction from the current room
+     */
 	public void setDir(String direction, Room room) {
 		dir.put(direction, room);
 	}
-
-
-	/**
-     * Methods for added means if getting into above or below rooms.
-     */
-    //public void setAbove(Room above) { this.above = above; }
-    //public void setBelow(Room below) { this.below = below; }
 	
     /**
      * Retrieve a description of this room (to the user).
      */
     public String getDescription() { return description; }
-    
-    /**
-     * Methods to determine the rooms to which various
-     * doors-- if they exist-- lead.
-     */
-    //public Room getNorth() { return north; }
-    //public Room getSouth() { return south; }
-    //public Room getEast() { return east; }
-    //public Room getWest() { return west; }
-    
-    /**
-     * Adds the ability to retrieve the above and below room, if there is any
-     */
-    //public Room getAbove() { return above; }
-    //public Room getBelow() { return below; }
-    
-
 	
 }
