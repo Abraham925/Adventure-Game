2b99461b4b56652508a06d7880e4161953751e19
Josh <josh.elsen@my.wheaton.edu>
2023-03-14 19:31:22 -0500

    Added to the skeleton of the ship

--- game/Game.java
+++ game/Game.java
@@ -195,17 +195,59 @@ package game;
         hallwaySouth.setDir("cargo", cargoBay);
         hallwayEastSouth.setDir("light", flickeringLight);
         
-        //floor 3 set up
+        //basement
+        Room basementCollapse = new Room ("Around you is a labryth of pipes and machines, the inner workings of the ship. "
+        		+ "The hallway above you is too far to get back up to.");
+        		//add roomJoiner for button to open up stair for basementBottomLeft
+        Room basementTopRight = new Room ("There is a flashing button labeled HANGAR on the wall."
+        		+ "There is what looks like a boiler next to you.");
+        		//add item for part of the ship
+        Room basementBottomRight = new Room ("You find a massive machine reaching up into the center of the ship."
+        		+ "All the pipes around you seem to be leading into it.");
+        Room basementBottomLeft = new Room ("There are some slabs slightly protruding from the wall, just barely enough to see."
+        		+ " They seem to lead to a trapdoor.");
+        basementCollapse.setDir("left", basementBottomLeft);
+        basementCollapse.setDir("right", basementTopRight);
+        basementBottomLeft.setDir("left", basementBottomRight);
+        basementBottomLeft.setDir("right", basementCollapse);
+        basementBottomRight.setDir("left", basementTopRight);
+        basementBottomRight.setDir("right", basementBottomLeft);
+        basementTopRight.setDir("left", basementCollapse);
+        basementTopRight.setDir("right", basementBottomRight);
+        
+        //floor 2
+        Room floor2Entrance = new Room ("Ahead of you you see a large bay window and some equipment.");
+        Room floor2SouthEast = new Room ("There's a sound almost like wind coming from a hall to your left.");
+        Room floor2SouthWest = new Room ("You see a door in the hallway to your left with a yellow light coming out of it.");
+        Room floor2West = new Room ("Through the blurry door of the door you see something green");
+        Room floor2NorthWest = new Room ("You see a door in the hallway to your left with a yellow light coming out of it.");
+        Room floor2NorthEast = new Room ("There's a sound almost like wind coming from a hall to your right.");
+        Room floor2Hall = new Room ("Looking into the large room ahead of you, you see what looks like a turret."
+        		+ "There is a hall to your left.");
+        Room floor2Turret = new Room ("There's a large cannon you can see from the side of the bay window. There is"
+        		+ " a chair surrounded by equitment that seems to be able to operate it.");
+        Room floor2Bio = new Room ("You are surrounded by plants from all over the galaxy, and containers with some fruits"
+        		+ " still left in them.");
+        
+        //floor 3
         Room elevatorExitF3 = new Room ("You see a hallway illuminated by a red light eminating from the end of the hall."
         		+ "You see the elevator on the other side of the gap in the floor.");
         Room hallway1F3 = new Room ("You can vaguely make out something standing next to the red light."
         		+ "There is a door next to you.");
         //ITEM
         Room hallway1DoorF3 = new Room ("Inside the room there are some strange looking plants growing in glass containers."
-        		+ "Most of the containers are broken or shattered, but on is still intact.");
+        		+ "Most of the containers are broken or shattered, but one is still intact.");
         Room hallway2F3 = new Room ("The red light seems to be coming from an panel in a room at the end of the hall."
-        		+ "The form appears to be human.");
-        Room hallway3F3 = new Room ("You see something flashing next to the red light on the panel. The form isn't moving.");
+        		+ "There is another hall next to you.");
+        Room blackhall1 = new Room ("There are panels alongside the wall that seem to be measuring something.");
+        Room blackhall2 = new Room ("There is a faint hum. Strangly, it seems to be coming from all around you.");
+        Room blackhall3 = new Room ("In front of you is a large console and a pinkish swirling mass on the otherside of a very thick,"
+        		+ "very broken piece of glass. Large pieces lie shattered on the ground. The hum is louder.");
+        Room blackholeWarpdrive = new Room ("As you approach it the swirling mass, the hum grows louder and louder until"
+        		+ "it begins to hurt. You feel the stange urge to reach your hand out...");
+        Room hallway3F3 = new Room ("You see something flashing next to the red light on the panel. "
+        		+ "The form isn't moving.");
+        
         
         //space station end
         currentRoom = hangar;   
